0149cda3217bb50b13ce5b921ee0a64b59ec2469
Karuto Z <iamkaruto@gmail.com>
2014-03-24 00:28:37 -0700

    Fixed random generation

--- main.c
+++ main.c
@@ -47,76 +47,13 @@ pthread_cond_t ok_to_proceed;
 // Function headers
 void Usage(char* prog_name);
 void Print_list(int *l, int size, char *name);
+int Is_used(int seed, int offset, int range);
 void *Thread_work(void* rank);
 
 // Global variables
 int i, thread_count, sample_size, list_size;
 int *list, *sample_keys, *sorted_keys;
 char *input_file;
-
-/*--------------------------------------------------------------------*/
-int main(int argc, char* argv[]) {
-  long thread;
-  pthread_t* thread_handles; 
-  double start, finish;
-
-  // for (int i = 0; i < argc; ++i){
-  //   printf("Command line args === argv[%d]: %s\n", i, argv[i]);
-  // }  
-
-  if (argc != 5) { 
-	Usage(argv[0]);
-  } else { // TODO: process optional suppress command
-  	thread_count = strtol(argv[1], NULL, 10);
-  	sample_size = strtol(argv[2], NULL, 10);
-  	list_size = strtol(argv[3], NULL, 10);
-  	input_file = argv[4];
-  }
-
-  // Allocate memory for variables
-  thread_handles = malloc(thread_count*sizeof(pthread_t));
-  list = malloc(list_size * sizeof(int));
-  sample_keys = malloc(sample_size * sizeof(int));
-  sorted_keys = malloc(sample_size * sizeof(int));
-  
-
-  pthread_mutex_init(&barrier_mutex, NULL);
-  pthread_cond_init(&ok_to_proceed, NULL);
-  
-
-  // Read list content from input
-  FILE *fp = fopen(input_file, "r+");
-  for (i = 0; i < list_size; i++) {
-  	  if (!fscanf(fp, "%d", &list[i])) {
-    	  break;
-      }
-  }
-  Print_list(list, list_size, "original list");
-  Print_list(sample_keys, sample_size, "sample keys");
-  
-  GET_TIME(start);
-  
-  for (thread = 0; thread < thread_count; thread++)
-     pthread_create(&thread_handles[thread], NULL,
-         Thread_work, (void*) thread);
-
-  for (thread = 0; thread < thread_count; thread++) 
-     pthread_join(thread_handles[thread], NULL);
-  
-  GET_TIME(finish);
-  
-  Print_list(sample_keys, sample_size, "sample keys (unsorted)");
-  Print_list(sorted_keys, sample_size, "sample keys (sorted)");
-  
-  // printf("Elapsed time = %e seconds\n", finish - start);
-
-
-  pthread_mutex_destroy(&barrier_mutex);
-  pthread_cond_destroy(&ok_to_proceed);
-  free(thread_handles);
-  
-  return 0;
-}  /* main */
 
 
 
@@ -150,6 +87,24 @@ void Print_list(int *l, int size, char *name) {
 
 
 
+/*--------------------------------------------------------------------
+ * Function:    Is_used
+ * Purpose:     Check if the random seeded key is already selected in sample
+ * In arg:      seed, offset, range
+ */
+int Is_used(int seed, int offset, int range) {
+	for (int i = offset; i < (offset + range); i++) {
+		if (sample_keys[i] == list[seed]) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
+} /* Is_used */
+
+
+
 /*-------------------------------------------------------------------
  * Function:    Thread_work
  * Purpose:     Run BARRIER_COUNT barriers
@@ -159,7 +114,7 @@ void Print_list(int *l, int size, char *name) {
  */
 void *Thread_work(void* rank) {
   long my_rank = (long) rank;
-  int i, m, seed, dup, index, offset, local_chunk_size, local_sample_size;
+  int i, seed, index, offset, local_chunk_size, local_sample_size;
 
   local_chunk_size = list_size / thread_count;
   local_sample_size = sample_size / thread_count;
@@ -170,33 +125,24 @@ void *Thread_work(void* rank) {
   srandom(my_rank + 1);  
   offset = my_rank * local_sample_size;
   
-  for (i = 0; i < local_sample_size; i++) {
-	index = offset + i;
-	seed = (my_rank * local_chunk_size) + (random() % local_chunk_size);
-	printf("T%ld, seed = %d\n", my_rank, seed);
-  	printf("T%ld, index = %d, i = %d, key = %d, LCS = %d\n\n", my_rank, index, i, list[seed], local_sample_size);
+  for (i = offset; i < (offset + local_sample_size); i++) {
+	  do {
+		  // If while returns 1, you'll be repeating this
+		  seed = (my_rank * local_chunk_size) + (random() % local_chunk_size);
+	  } while (Is_used(seed, offset, local_sample_size));
+	  // If the loop breaks (while returns 0), data is clean, assignment
+	  sample_keys[i] = list[seed];
+	  index = offset + i;
+	  // printf("T%ld, seed = %d\n", my_rank, seed);
+	  // printf("T%ld, index = %d, i = %d, key = %d, LCS = %d\n\n", my_rank, index, i, list[seed], local_sample_size);
 	
-	dup = 0;
-	// Make sure this sample key is not already in the list
-	for (m = 0; m < local_sample_size; m++) {
-		if (sample_keys[index+m] == list[seed]) {
-			// If it already exists in [my section of] sample keys...
-			// at this round we must pick a new seed
-			seed = (my_rank * local_chunk_size) + (random() % local_chunk_size);
-			printf("T%ld, seed = %d [RESET]\n", my_rank, seed);
-			dup = 1;
-		}
-	}
-	// Up to this point we can put it into [unsorted] sample keys
-	if (dup != 1) {
-	    sample_keys[index] = list[seed];
-	}
+
   }
   
   // TODO: lock to force syncing
   /*
   // Parallel count sort the sample keys
-  for (i = 0; i < local_sample_size; i++) {
+  for (i = offset; i < (offset + local_sample_size); i++) {
 	  int mykey = sample_keys[offset + i];
 	  int myindex = 0;
 	  for (int j = 0; j < sample_size; j++) {
@@ -244,5 +190,67 @@ void *Thread_work(void* rank) {
 
 
 
+/*--------------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+  long thread;
+  pthread_t* thread_handles; 
+  double start, finish;
+
+  // for (int i = 0; i < argc; ++i){
+  //   printf("Command line args === argv[%d]: %s\n", i, argv[i]);
+  // }  
+
+  if (argc != 5) { 
+	Usage(argv[0]);
+  } else { // TODO: process optional suppress command
+  	thread_count = strtol(argv[1], NULL, 10);
+  	sample_size = strtol(argv[2], NULL, 10);
+  	list_size = strtol(argv[3], NULL, 10);
+  	input_file = argv[4];
+  }
+
+  // Allocate memory for variables
+  thread_handles = malloc(thread_count*sizeof(pthread_t));
+  list = malloc(list_size * sizeof(int));
+  sample_keys = malloc(sample_size * sizeof(int));
+  sorted_keys = malloc(sample_size * sizeof(int));
+  
+
+  pthread_mutex_init(&barrier_mutex, NULL);
+  pthread_cond_init(&ok_to_proceed, NULL);
+  
+
+  // Read list content from input
+  FILE *fp = fopen(input_file, "r+");
+  for (i = 0; i < list_size; i++) {
+  	  if (!fscanf(fp, "%d", &list[i])) {
+    	  break;
+      }
+  }
+  Print_list(list, list_size, "original list");
+  
+  GET_TIME(start);
+  
+  for (thread = 0; thread < thread_count; thread++)
+     pthread_create(&thread_handles[thread], NULL,
+         Thread_work, (void*) thread);
+
+  for (thread = 0; thread < thread_count; thread++) 
+     pthread_join(thread_handles[thread], NULL);
+  
+  GET_TIME(finish);
+  
+  Print_list(sample_keys, sample_size, "sample keys (unsorted)");
+  Print_list(sorted_keys, sample_size, "sample keys (sorted)");
+  
+  // printf("Elapsed time = %e seconds\n", finish - start);
+
+
+  pthread_mutex_destroy(&barrier_mutex);
+  pthread_cond_destroy(&ok_to_proceed);
+  free(thread_handles);
+  
+  return 0;
+}  /* main */
 
 
